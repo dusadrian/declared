11bb13779cb5998109e101a3c384b606747ec177
Adrian Dusa <dusa.adrian@gmail.com>
2022-02-26 15:35:22 +0200

    improved printing crosstabs

--- R/w_table.R
+++ R/w_table.R
@@ -117,19 +117,6 @@
             )
         }
         
-        if (isTRUE(values)) {
-            rownames(res) <- gsub("_-_", " ", rownames(res))
-            colnames(res) <- gsub("_-_", " ", colnames(res))
-        }
-        else {
-            labels <- rownames(res)
-            labels <- unlist(lapply(strsplit(labels, split = "_-_"), "[[", 1))
-            rownames(res) <- labels
-            labels <- colnames(res)
-            labels <- unlist(lapply(strsplit(labels, split = "_-_"), "[[", 1))
-            colnames(res) <- labels
-        }
-        
         if (is.null(margin) || margin != 1) {
             res <- rbind(res, Total = colSums(res))
         }
@@ -141,6 +128,11 @@
         if (length(margin)) {
             res <- round(100 * res, 1)
         }
+
+        attr(res, "xvalues") <- isTRUE(values) & xvalues
+        attr(res, "yvalues") <- isTRUE(values) & yvalues
+
+        class(res) <- c("w_table", "matrix")
     }
     else {
         labels <- rownames(tbl)
@@ -195,141 +187,207 @@
     irv <- c(194, 180)
     tick <- unlist(strsplit(rawToChar(as.raw(irv)), split = ""))
 
-    show_values <- attr(x, "show_values")
-    valid <- attr(x, "valid")
-    dots <- list(...)
-    if (is.element("show_values", names(dots))) {
-        show_values <- dots$show_values
-    }
+    if (is.matrix(x)) { # crosstab
 
-    values <- gsub(paste(tick, collapse = "|"), "'", attr(x, "values"))
-    labels <- gsub(paste(tick, collapse = "|"), "'", attr(x, "labels"))
-    na_values <- gsub(paste(tick, collapse = "|"), "'", attr(x, "na_values"))
-
-    first_missing <- Inf
-    if (any(is.element(values, na_values))) {
-        first_missing <- which(is.element(values, na_values))[1]
-    }
-
-    if (first_missing == Inf && is.na(labels[length(labels)])) {
-        first_missing <- length(labels)
-    }
-    
-    rnms <- labels
-    
-    if (show_values) {
-        values <- formatC(as.character(values), digits = max(nchar(values)) - 1, flag = " ")
-        labels[!is.na(labels)][values == labels[!is.na(labels)]] <- ""
-        rnms[!is.na(labels)] <- paste(labels[!is.na(labels)], values, sep = " ")
-    }
-
-    rnms[is.na(labels)] <- "NA"
-    
-    max.nchar.cases <- max(nchar(encodeString(rnms)))
-    # rnms <- sprintf(paste0("% ", max.nchar.cases, "s"), rnms)
-    for (i in seq(length(rnms))) {
-        if (nchar(rnms[i]) < max.nchar.cases) {
-            rnms[i] <- paste(c(rep(" ", max.nchar.cases - nchar(rnms[i])), rnms[i]), collapse = "", sep = "")
-        }
-    }
-
-    sums <- colSums(x[, 1:3])
-
-    fres <- formatC(as.character(c(x$fre, sums[1])), format = "s")
-    fres <- paste(sprintf(paste("%", max(3, nchar(sums[1])), "s", sep = ""), fres), "")
-    # fres <- format(c(paste(rep(1, max(4, nchar(sums[1]))), collapse = ""), fres), justify = "centre")[-1]
-    x$rel <- formatC(x$rel, digits = 3, format = "f")
-    rel <- sprintf("% 5s", x$rel)
-    x$per <- formatC(x$per, digits = 1, format = "f")
-    if (valid & is.element("vld", names(x))) {
-        x$vld <- formatC(x$vld, digits = 1, format = "f")
-        vld <- sprintf(paste("% 5s", sep = ""), x$vld)
-    }
-    per <- sprintf("% 5s", c(x$per, sums[3]))
-    cpd <- formatC(x$cpd, digits = 1, format = "f")
-    cpd <- sprintf(paste("% 5s", sep = ""), cpd)
-    
-    miseparator <- paste(
-        c(
-            rep(" ", ifelse(max.nchar.cases > 5, max.nchar.cases - 5, 0)),
-            rep("-", min(max.nchar.cases, 5) + 1 * (sums[1] >= 1000)),
-            "\n"
-        ),
-        collapse = ""
-    )
-    separator <- paste(
-        c(
-            rep(" ", max.nchar.cases + 1),
-            rep("-", nchar(sums[1])),
-            ifelse(
-                nchar(sums[1]) < 3, 
-                paste(
-                    rep("-", 3 - nchar(sums[1])),
-                    collapse = ""
-                ),
-                ""
-            ),
-            sprintf("-------------------%s\n", ifelse(valid, "------", ""))
-        ),
-        collapse = ""
-    )
-
-    if (nrow(x) > 100 & !force) {
-        admisc::stopError("It looks like a lot of categories. If you really want to print it, use:\nprint(x, force = TRUE)")
-    }
-
-    cat(ifelse(startend, "\n", ""))
-
-    cat(
-        paste(
-            rep(
-                " ",
-                max.nchar.cases + ifelse(nchar(sums[1]) > 4, nchar(sums[1]) - 4, 0)
-            ),
-            collapse = ""
-        ),
-        sprintf(ifelse(
-            sums[1] < 1000,
-            "fre    rel   per   %scpd\n",
-            " fre    rel   per   %scpd\n"
-        ), ifelse(valid, "vld   ", ""))
-    )
-
-    cat(separator)
-
-    for (i in seq(nrow(x))) {
-        if (first_missing == i) {
-            cat(miseparator)
+        rnms <- strsplit(rownames(x), split = "_-_")
+        
+        xlabels <- unlist(lapply(rnms, "[[", 1))
+        max.nchar.xlabels <- max(nchar(encodeString(xlabels)))
+        for (i in seq(length(xlabels))) {
+            if (nchar(xlabels[i]) < max.nchar.xlabels) {
+                xlabels[i] <- admisc::padLeft(xlabels[i], max.nchar.xlabels - nchar(xlabels[i]))
+            }
         }
 
-        if (valid) {
-            if (i < first_missing) {
-                cat(rnms[i], fres[i], rel[i], per[i], vld[i], cpd[i], "\n")
+
+        if (attr(x, "xvalues")) {
+            # -length(rnms), because of "Total" which does not have a value
+            xvalues <- unlist(lapply(rnms[-length(rnms)], "[[", 2))
+            xvalues <- c(xvalues, "")
+
+            max.nchar.xvalues <- max(nchar(encodeString(xvalues)))
+
+            for (i in seq(length(xvalues) - 1)) {
+                if (nchar(xvalues[i]) < max.nchar.xvalues) {
+                    xvalues[i] <- admisc::padLeft(xvalues[i], max.nchar.xvalues - nchar(xvalues[i]))
+                }
             }
-            else {
-                cat(rnms[i], fres[i], rel[i], per[i], "\n")
-            }
+
+            rnms <- paste(xlabels, xvalues)
         }
         else {
-            cat(rnms[i], fres[i], rel[i], per[i], cpd[i], "\n")
+            rnms <- xlabels
         }
+
+        rownames(x) <- rnms
+
+        cnms <- colnames(x)
+        if (attr(x, "yvalues")) {
+            cnms <- gsub("_-_", " ", cnms)
+        }
+        else {
+            cnms <- unlist(lapply(strsplit(cnms, split = "_-_"), "[[", 1))
+        }
+
+        max.nchar.cols <- max(nchar(encodeString(cnms)))
+        for (i in seq(length(cnms))) {
+            if (nchar(cnms[i]) < max.nchar.cols) {
+                cnms[i] <- admisc::padBoth(cnms[i], max.nchar.cols - nchar(cnms[i]))
+            }
+        }
+        colnames(x) <- cnms
+
+        for (i in seq(length(x))) {
+            if (nchar(x[i]) < max.nchar.cols) {
+                x[i] <- admisc::padBoth(x[i], max.nchar.cols - nchar(x[i]))
+            }
+        }
+        
+        class(x) <- setdiff(class(x), "w_table")
+        attr(x, "xvalues") <- NULL
+        attr(x, "yvalues") <- NULL
+        cat("\n")
+        print(noquote(x))
+        cat("\n")
     }
+    else {
+        show_values <- attr(x, "show_values")
+        valid <- attr(x, "valid")
+        dots <- list(...)
+        if (is.element("show_values", names(dots))) {
+            show_values <- dots$show_values
+        }
 
-    cat(separator)
+        values <- gsub(paste(tick, collapse = "|"), "'", attr(x, "values"))
+        labels <- gsub(paste(tick, collapse = "|"), "'", attr(x, "labels"))
+        na_values <- gsub(paste(tick, collapse = "|"), "'", attr(x, "na_values"))
 
-    # cat(paste(rep(" ", max.nchar.cases), sep = ""), " ", sprintf(paste("% ", max(4, nchar(sums[1])), "s", sep = ""), sums[1]), " 1.000 100.0\n", sep = "")
-    cat(
-        paste(
-            rep(" ", max.nchar.cases),
+        first_missing <- Inf
+        if (any(is.element(values, na_values))) {
+            first_missing <- which(is.element(values, na_values))[1]
+        }
+
+        if (first_missing == Inf && is.na(labels[length(labels)])) {
+            first_missing <- length(labels)
+        }
+        
+        rnms <- labels
+        
+        if (show_values) {
+            values <- formatC(as.character(values), digits = max(nchar(values)) - 1, flag = " ")
+            labels[!is.na(labels)][values == labels[!is.na(labels)]] <- ""
+            rnms[!is.na(labels)] <- paste(labels[!is.na(labels)], values, sep = " ")
+        }
+
+        rnms[is.na(labels)] <- "NA"
+        
+        max.nchar.cases <- max(nchar(encodeString(rnms)))
+        # rnms <- sprintf(paste0("% ", max.nchar.cases, "s"), rnms)
+        for (i in seq(length(rnms))) {
+            if (nchar(rnms[i]) < max.nchar.cases) {
+                rnms[i] <- admisc::padLeft(rnms[i], max.nchar.cases - nchar(rnms[i]))
+                # rnms[i] <- paste(c(rep(" ", max.nchar.cases - nchar(rnms[i])), rnms[i]), collapse = "", sep = "")
+            }
+        }
+
+        sums <- colSums(x[, 1:3])
+
+        fres <- formatC(as.character(c(x$fre, sums[1])), format = "s")
+        fres <- paste(sprintf(paste("%", max(3, nchar(sums[1])), "s", sep = ""), fres), "")
+        # fres <- format(c(paste(rep(1, max(4, nchar(sums[1]))), collapse = ""), fres), justify = "centre")[-1]
+        x$rel <- formatC(x$rel, digits = 3, format = "f")
+        rel <- sprintf("% 5s", x$rel)
+        x$per <- formatC(x$per, digits = 1, format = "f")
+        if (valid & is.element("vld", names(x))) {
+            x$vld <- formatC(x$vld, digits = 1, format = "f")
+            vld <- sprintf(paste("% 5s", sep = ""), x$vld)
+        }
+        per <- sprintf("% 5s", c(x$per, sums[3]))
+        cpd <- formatC(x$cpd, digits = 1, format = "f")
+        cpd <- sprintf(paste("% 5s", sep = ""), cpd)
+        
+        miseparator <- paste(
+            c(
+                rep(" ", ifelse(max.nchar.cases > 5, max.nchar.cases - 5, 0)),
+                rep("-", min(max.nchar.cases, 5) + 1 * (sums[1] >= 1000)),
+                "\n"
+            ),
+            collapse = ""
+        )
+        separator <- paste(
+            c(
+                rep(" ", max.nchar.cases + 1),
+                rep("-", nchar(sums[1])),
+                ifelse(
+                    nchar(sums[1]) < 3, 
+                    paste(
+                        rep("-", 3 - nchar(sums[1])),
+                        collapse = ""
+                    ),
+                    ""
+                ),
+                sprintf("-------------------%s\n", ifelse(valid, "------", ""))
+            ),
+            collapse = ""
+        )
+
+        if (nrow(x) > 100 & !force) {
+            admisc::stopError("It looks like a lot of categories. If you really want to print it, use:\nprint(x, force = TRUE)")
+        }
+
+        cat(ifelse(startend, "\n", ""))
+
+        cat(
+            paste(
+                rep(
+                    " ",
+                    max.nchar.cases + ifelse(nchar(sums[1]) > 4, nchar(sums[1]) - 4, 0)
+                ),
+                collapse = ""
+            ),
+            sprintf(ifelse(
+                sums[1] < 1000,
+                "fre    rel   per   %scpd\n",
+                " fre    rel   per   %scpd\n"
+            ), ifelse(valid, "vld   ", ""))
+        )
+
+        cat(separator)
+
+        for (i in seq(nrow(x))) {
+            if (first_missing == i) {
+                cat(miseparator)
+            }
+
+            if (valid) {
+                if (i < first_missing) {
+                    cat(rnms[i], fres[i], rel[i], per[i], vld[i], cpd[i], "\n")
+                }
+                else {
+                    cat(rnms[i], fres[i], rel[i], per[i], "\n")
+                }
+            }
+            else {
+                cat(rnms[i], fres[i], rel[i], per[i], cpd[i], "\n")
+            }
+        }
+
+        cat(separator)
+
+        # cat(paste(rep(" ", max.nchar.cases), sep = ""), " ", sprintf(paste("% ", max(4, nchar(sums[1])), "s", sep = ""), sums[1]), " 1.000 100.0\n", sep = "")
+        cat(
+            paste(
+                rep(" ", max.nchar.cases),
+                sep = ""
+            ),
+            " ",
+            fres[length(fres)],
+            " 1.000 100.0\n",
             sep = ""
-        ),
-        " ",
-        fres[length(fres)],
-        " 1.000 100.0\n",
-        sep = ""
-    )
+        )
 
-    cat(ifelse(startend, "\n", ""))
+        cat(ifelse(startend, "\n", ""))
+    }
 }
 
 
