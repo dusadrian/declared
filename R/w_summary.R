814c0c4bad71172785f4a964a527e49b22afcf95
Adrian Dusa <dusa.adrian@gmail.com>
2022-03-04 00:51:39 +0200

    new function w_summary()

---
+++ R/w_summary.R
@@ -0,0 +1,24 @@
+`w_summary` <- function(
+    x, wt = NULL, ...
+) {
+
+    wsum <- w_quantile(x, wt = wt, ... = ...)
+    wmean <- w_mean(x, wt = wt, ... = ...)
+    wsum <- c(wsum[1:3], wmean, wsum[4:5])
+
+    if (inherits(x, "haven_labelled")) {
+        x <- as_declared(x)
+    }
+
+    nas <- w_table(is.na(undeclare(x)), wt = wt)
+    rnms <- rownames(nas)
+
+    names(wsum) <- c("Min.", "1st Qu.", "Median", "Mean", "3rd Qu.", "Max.")
+
+    if (any(rnms == "TRUE")) {
+        wsum <- c(wsum, "NA's" = nas$fre[2])
+    }
+
+    class(wsum) <- c("admisc_fobject", class(wsum))
+    return(wsum)
+}
