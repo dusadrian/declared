4584c5e50e718010cb5ba7509d9126facff20801
Adrian Dusa <dusa.adrian@gmail.com>
2022-06-18 21:04:17 +0300

    new argument measurement

--- R/declared.R
+++ R/declared.R
@@ -202,14 +202,16 @@
 
 
 `declared` <- function(
-    x, labels = NULL, na_values = NULL, na_range = NULL, label = NULL, ...
+    x, labels = NULL, na_values = NULL, na_range = NULL, label = NULL, 
+    measurement = NULL, ...
 ) {
     UseMethod("declared")
 }
 
 
 `declared.default` <- function(
-    x, labels = NULL, na_values = NULL, na_range = NULL, label = NULL, ...
+    x, labels = NULL, na_values = NULL, na_range = NULL, label = NULL,
+    measurement = NULL, ...
 ) {
     if (is.factor(x)) {
         nms <- levels(x)
@@ -283,43 +285,11 @@
     attr(x, "labels") <- labels
     attr(x, "label") <- label
 
+    attr(x, "measurement") <- check_measurement(measurement)
+
     return(x)
 }
 
-
-`missingValues` <- function(x) {
-    
-    mv <- rep(NA, length(x))
-    
-    if (is.declared(x)) {
-        misvals <- attr(x, "na_index")
-        mv[as.numeric(names(misvals))] <- misvals
-    }
-
-    return(mv)
-}
-
-
-`missingValues<-` <- function(x, value) {
-    
-    class(x) <- setdiff(class(x), "declared")
-    other_classes <- setdiff(class(x), c("integer", "double", "character", "numeric", "complex", "haven_labelled", "haven_labelled_spss", "vctrs_vctr"))
-    
-    notna <- !is.na(value)
-    x[notna] <- NA
-
-    if (!all(is.na(x))) {
-        x <- coerceMode_(x)
-    }
-
-    if (any(notna)) {
-        na_index <- which(notna)
-        names(na_index) <- value[notna]
-        attr(x, "na_index") <- na_index
-    }
-    
-    structure(x, class = c("declared", other_classes, class(x)))
-}
 
 `as.character.declared` <- function(x, values = FALSE, ...) {
 
