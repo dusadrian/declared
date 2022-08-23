868c27efb7f38aa530aa5d3cb1026bfdfb263892
Adrian Dusa <dusa.adrian@gmail.com>
2022-08-23 16:33:53 +0300

    changed tests for undeclare()

--- tests/testthat/test-undeclare.R
+++ tests/testthat/test-undeclare.R
@@ -1,3 +1,28 @@
+x <- declared(
+    c(1:5, -1),
+    labels = c(Good = 1, Bad = 5, DK = -1),
+    na_values = -1
+)
+
+na_index <- attr(x, "na_index")
+attrx <- attributes(x)
+ux <- x
+attributes(ux) <- NULL
+ux[na_index] <- names(na_index)
+ux <- as.integer(ux)
+
+
+
+test_that("undeclare() dropping attributes works", {
+  expect_true(identical(undeclare(x, drop = TRUE), ux))
+})
+
+
+attrx$na_index <- NULL
+attrx$na_values <- NULL
+attrx$na_range <- NULL
+attributes(ux) <- attrx
+
 test_that("undeclare() works", {
-  expect_true(is.numeric(undeclare(x)))
+  expect_true(identical(undeclare(x), ux))
 })
