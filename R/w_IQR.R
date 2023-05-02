d6d04885ab3dfbf5c4a8f3823d9cf12ab731cdc4
Adrian Dușa <dusa.adrian@gmail.com>
2023-05-03 00:57:22 +0300

    weighted inter quartile range

---
+++ R/w_IQR.R
@@ -0,0 +1,17 @@
+#' @rdname weighted
+#' @export
+`w_IQR` <- function (
+    x, wt = NULL, na.rm = FALSE, ...
+) {
+
+    unclass(diff (
+        w_quantile(
+            x,
+            wt = wt,
+            probs = c(0.25, 0.75),
+            na.rm = na.rm,
+            names = FALSE,
+            ... = ...
+        )
+    ))
+}
