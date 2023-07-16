545680c5b6ec4a4ce23aa1dfac295ee0335fa18e
Adrian Dușa <dusa.adrian@gmail.com>
2023-07-16 12:35:21 +0300

    more tests for all_missing_values() with na_range

--- tests/testthat/test-internals.R
+++ tests/testthat/test-internals.R
@@ -23,6 +23,15 @@ test_that("anyTagged_() works", {
 })
 
 
+test_that("all_missing_values() works", {
+  expect_equal(all_missing_values(c(1:5, -91), na_values = -91), -91)
+  expect_equal(
+    all_missing_values(c(1:5, -91), na_range = c(-91, -99)),
+    all_missing_values(c(1:5, -91), na_range = c(-99, -91))
+  )
+  expect_error(all_missing_values(c(1:5, -91), na_range = c(-91, -91)))
+})
+
 
 test_that("format_declared() works", {
   expect_length(format_declared(x), 6)
