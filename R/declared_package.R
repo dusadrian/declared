bb486b49f82c05742cab532be2c9dbbc9e4f5325
Adrian Dușa <dusa.adrian@gmail.com>
2023-11-26 00:07:37 +0200

    up minor version

--- R/declared_package.R
+++ R/declared_package.R
@@ -24,8 +24,8 @@
 #' \tabular{ll}{
 #'   Package: \tab declared\cr
 #'   Type: \tab Package\cr
-#'   Version: \tab 0.23\cr
-#'   Date: \tab 2023-10-01\cr
+#'   Version: \tab 0.23.1\cr
+#'   Date: \tab 2023-11-25\cr
 #'   License: \tab GPL-v3\cr
 #' }
 #'
